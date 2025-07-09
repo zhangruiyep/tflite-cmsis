07eccc31bc12954373c76ad16574b801cbd650ad
jiqunli <jiqunli@sifli.com>
2025-07-09 17:49:30 +0800

    using mic speeech

--- examples/kws/kws.cc
+++ examples/kws/kws.cc
@@ -39,7 +39,7 @@ namespace {
 // Arena size is a guesstimate, followed by use of
 // MicroInterpreter::arena_used_bytes() on both the AudioPreprocessor and
 // MicroSpeech models and using the larger of the two results.
-constexpr size_t kArenaSize = 300*1024;  
+constexpr size_t kArenaSize = 300*1024;
 
 ALIGN(16)
 uint8_t g_arena[kArenaSize];
@@ -72,10 +72,10 @@ TfLiteStatus GenerateFeatures(const int16_t* data,
                               const size_t size,
                               float quantScale,
                               float quantOffset,
-                              Features* features_output) 
+                              Features* features_output)
 {
     std::vector<float> audioData(size);
-    
+
      //DS-CNN model settings
     float SAMP_FREQ = kAudioSampleFrequency;
     int MFCC_WINDOW_LEN = kFeatureDurationMs*kAudioSampleFrequency/1000;
@@ -83,7 +83,7 @@ TfLiteStatus GenerateFeatures(const int16_t* data,
     int NUM_MFCC_FEATS = kFeatureSize;
     int NUM_MFCC_VECTORS = kFeatureCount;
     //todo: calc in pipeline and use in main
-    int SAMPLES_PER_INFERENCE = NUM_MFCC_VECTORS * MFCC_WINDOW_STRIDE + 
+    int SAMPLES_PER_INFERENCE = NUM_MFCC_VECTORS * MFCC_WINDOW_STRIDE +
                                 MFCC_WINDOW_LEN - MFCC_WINDOW_STRIDE; //16000
     float MEL_LO_FREQ = 20;
     float MEL_HI_FREQ = 4000;
@@ -96,9 +96,9 @@ TfLiteStatus GenerateFeatures(const int16_t* data,
                           NUM_MFCC_FEATS,
                           MFCC_WINDOW_LEN, false,
                           NUM_MFCC_VECTORS);
-    
-    std::unique_ptr<MFCC> mfccInst = std::make_unique<MFCC>(mfccParams);  
-    
+
+    std::unique_ptr<MFCC> mfccInst = std::make_unique<MFCC>(mfccParams);
+
     for (size_t i = 0; i < size; ++i) {
         // 将int16_t范围[-32768, 32767]归一化到float范围[-1.0, 1.0)
         audioData[i] = static_cast<float>(data[i]) / 32768.0f;
@@ -109,18 +109,18 @@ TfLiteStatus GenerateFeatures(const int16_t* data,
                 audioData.data() + i*MFCC_WINDOW_STRIDE,  // 使用data()获取指针
                 audioData.data() + i*MFCC_WINDOW_STRIDE + MFCC_WINDOW_LEN
         );
-        
+
         //MicroPrintf("GenerateFeatures 3, %d, %f, %f",i, quantScale, quantOffset);
         auto mfcc = mfccInst->MfccComputeQuant<int8_t>(mfccAudioData, quantScale, quantOffset);
 
         for (size_t j = 0; j < mfcc.size() && j < 10; ++j) {
             (*features_output)[i][j] = static_cast<int8_t>(mfcc[j]);
         }
-    }   
+    }
     return kTfLiteOk;
 }
 
-TfLiteStatus LoadMicroSpeechModel(    ) 
+TfLiteStatus LoadMicroSpeechModel(    )
 {
     // Map the model into a usable data structure. This doesn't involve any
     // copying or parsing, it's a very lightweight operation.
@@ -144,44 +144,44 @@ TfLiteStatus LoadMicroSpeechModel(    )
 }
 
 
-TfLiteStatus UnloadModel(    ) 
+TfLiteStatus UnloadModel(    )
 {
     delete g_interpreter;
     g_interpreter = nullptr;
-    
+
     return kTfLiteOk;
 }
 
 TfLiteStatus PerformInference(
-    const int16_t* audio_data, const size_t audio_data_size, const char* expected_label) 
+    const int16_t* audio_data, const size_t audio_data_size, const char* expected_label)
 {
-  
+
     TfLiteTensor* input = g_interpreter->input(0);
     TF_LITE_MICRO_EXPECT(input != nullptr);
-    TF_LITE_MICRO_CHECK_FAIL();
+    //TF_LITE_MICRO_CHECK_FAIL();
     // check input shape is compatible with our feature data size
     TF_LITE_MICRO_EXPECT_EQ(kFeatureElementCount,
                           input->dims->data[input->dims->size - 1]);
-    TF_LITE_MICRO_CHECK_FAIL();
-    
+    //TF_LITE_MICRO_CHECK_FAIL();
+
     TfLiteTensor* output = g_interpreter->output(0);
     TF_LITE_MICRO_EXPECT(output != nullptr);
-    TF_LITE_MICRO_CHECK_FAIL();
+    //TF_LITE_MICRO_CHECK_FAIL();
     // check output shape is compatible with our number of prediction categories
     TF_LITE_MICRO_EXPECT_EQ(kCategoryCount,
                           output->dims->data[output->dims->size - 1]);
-    TF_LITE_MICRO_CHECK_FAIL();
+    //TF_LITE_MICRO_CHECK_FAIL();
 
     Features m_features;
     GenerateFeatures(audio_data, audio_data_size, input->params.scale, input->params.zero_point, &m_features);
-    
+
     float output_scale = output->params.scale;
     int output_zero_point = output->params.zero_point;
 
     std::copy_n(&m_features[0][0], kFeatureElementCount,
               tflite::GetTensorData<int8_t>(input));
     TF_LITE_MICRO_EXPECT(g_interpreter->Invoke() == kTfLiteOk);
-    TF_LITE_MICRO_CHECK_FAIL();
+    //TF_LITE_MICRO_CHECK_FAIL();
 
     // Dequantize output values
     float category_predictions[kCategoryCount];
@@ -201,7 +201,7 @@ TfLiteStatus PerformInference(
         prediction_index=kLabelUnknownIdx;
     TF_LITE_MICRO_EXPECT_STRING_EQ(expected_label,
                                  kCategoryLabels[prediction_index]);
-    TF_LITE_MICRO_CHECK_FAIL();
+    //TF_LITE_MICRO_CHECK_FAIL();
 
     return kTfLiteOk;
 }
@@ -215,11 +215,133 @@ TfLiteStatus TestAudioSample(const char* label, const int16_t* audio_data,
 
 }  // namespace
 
+#include <audio_server.h>
+#include "webrtc/common_audio/vad/include/webrtc_vad.h"
+
+
+#define MIC_1000MS_DATA_BYTES       (16000 * 2)
+
+static int test_using_mic = 1;
+typedef struct
+{
+    audio_client_t  client;
+    VadInst         *vad;
+    uint8_t         data[MIC_1000MS_DATA_BYTES + 320];
+    uint32_t        offset;
+    int             is_vad_started;
+    rt_event_t      event;
+} kws_handle_t;
+
+static kws_handle_t *thiz;
+
+static int kws_record_callback(audio_server_callback_cmt_t cmd, void *callback_userdata, uint32_t reserved)
+{
+    if (cmd == as_callback_cmd_data_coming)
+    {
+        audio_server_coming_data_t *p = (audio_server_coming_data_t *)reserved;
+        RT_ASSERT(p->data_len == 320);
+        if (thiz->is_vad_started == 1)
+        {
+            memcpy(&thiz->data[thiz->offset], p->data, p->data_len);
+            thiz->offset += p->data_len;
+            if (thiz->offset >= MIC_1000MS_DATA_BYTES)
+            {
+                thiz->is_vad_started = 2;
+                thiz->offset = 0;
+                rt_event_send(thiz->event, 1);
+            }
+        }
+        else if (thiz->is_vad_started == 2)
+        {
+            //kws is busy
+        }
+        else
+        {
+            int ret;
+            ret = WebRtcVad_Process(thiz->vad, 16000, (int16_t*)p->data, p->data_len/2);
+            if (ret)
+            {
+                MicroPrintf("voice start");
+                memcpy(&thiz->data[thiz->offset], p->data, p->data_len);
+                thiz->offset += p->data_len;
+                thiz->is_vad_started = 1;
+            }
+            else
+            {
+                //reserved 10ms data before vad detected
+                memcpy(&thiz->data[0], p->data, p->data_len);
+                thiz->offset = p->data_len;
+            }
+        }
+    }
+
+    return 0;
+}
+static kws_handle_t * kws_open()
+{
+    int ret;
+    RT_ASSERT(!thiz);
+    thiz = (kws_handle_t *)rt_malloc(sizeof(kws_handle_t));
+    RT_ASSERT(thiz);
+    thiz->offset = 0;
+    thiz->event = rt_event_create("kws", RT_IPC_FLAG_FIFO);
+    RT_ASSERT(thiz->event);
+    ret = WebRtcVad_Create(&thiz->vad);
+    RT_ASSERT(!ret);
+    ret = WebRtcVad_Init(thiz->vad);
+    RT_ASSERT(!ret);
+    ret = WebRtcVad_set_mode(thiz->vad, 1); // 0 ~ 3
+    RT_ASSERT(!ret);
+
+    audio_parameter_t pa = {0};
+    /*open audio record*/
+    pa.write_bits_per_sample = 16;
+    pa.write_channnel_num = 1;
+    pa.write_samplerate = kAudioSampleFrequency;
+    pa.write_cache_size = 4;
+    pa.read_bits_per_sample = 16;
+    pa.read_channnel_num = 1;
+    pa.read_samplerate = kAudioSampleFrequency;
+    pa.read_cache_size = 0;
+    thiz->client = audio_open(AUDIO_TYPE_LOCAL_RECORD, AUDIO_RX, &pa, kws_record_callback, (void *)NULL);
+    RT_ASSERT(thiz->client);
+    MicroPrintf("kws_open done");
+    return thiz;
+}
+static void kws_close()
+{
+    RT_ASSERT(thiz);
+    audio_close(thiz->client);
+    if (thiz->vad)
+    {
+        WebRtcVad_Free(thiz->vad);
+        thiz->vad = NULL;
+    }
+    rt_event_delete(thiz->event);
+    rt_free(thiz);
+    thiz = NULL;
+}
+
 TF_LITE_MICRO_TESTS_BEGIN
+
+rt_thread_mdelay(5000);
 
 TF_LITE_MICRO_TEST(Init) {
   LoadMicroSpeechModel();
 }
+
+
+kws_open();
+while (test_using_mic)
+{
+    MicroPrintf("please speaking\n");
+    rt_uint32_t evt = 0;
+    rt_event_recv(thiz->event, 1, RT_EVENT_FLAG_OR | RT_EVENT_FLAG_CLEAR, RT_WAITING_FOREVER, &evt);
+    TestAudioSample("yes", (const int16_t*)&thiz->data[0], MIC_1000MS_DATA_BYTES / 2);
+    thiz->is_vad_started = 0;
+}
+kws_close();
+
 
 TF_LITE_MICRO_TEST(NoTest) {
   TestAudioSample("no", g_no_1000ms_audio_data, g_no_1000ms_audio_data_size);
@@ -244,3 +366,17 @@ TF_LITE_MICRO_TEST(DeInit) {
 }
 
 TF_LITE_MICRO_TESTS_END
+
+int kws_mic(int argc, char **argv)
+{
+    test_using_mic = !test_using_mic;
+    return 0;
+}
+
+MSH_CMD_EXPORT_ALIAS(kws_mic, kws_mic, kws_mic);
+
+
+
+
+
+
